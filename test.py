ef20b445cbc0c74ab7358676b0354a720862a535
Chebotarev-Alexey <66257265+Chebotarev-Alexey@users.noreply.github.com>
2022-07-11 23:02:04 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from light_telegram_bot import Bot, BotPolling, NextHandler
 from config import token
+import asyncio
 
 async def main():
     bot = await Bot(token)
@@ -8,10 +9,22 @@ async def main():
 
     @polling.handler
     async def echo_messages(update):
-        if hasattr(update, "message"):
-        	raise NextHandler()
-        message = update.message
-        await bot.send_message(chat_id=message.from_.id, text=message.text)
+        if not "message" in update: raise NextHandler()
+        message = update["message"]
+        await bot.send_message(
+            chat_id=message["chat"]["id"], 
+            text=message["text"]
+            )
+    
+    @polling.handler
+    async def message_edited(update):
+        if not "edited_message" in update: raise NextHandler()
+        edited_message = update["edited_message"]
+        await bot.send_message(
+            chat_id=edited_message["chat"]["id"], 
+            text="Why did you edit this message??", 
+            reply_to_message_id=edited_message["message_id"]
+            )
 
     await polling.start()
 
